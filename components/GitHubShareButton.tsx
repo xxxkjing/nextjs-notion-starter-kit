b1c6ad3f3e720795979d60dc9bea4c90b09c7fed
MinusTen <xkjing@outlook.com>
2024-12-22 14:59:46 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://dgithub.xyz/xxxkjing'
+      href='https://github.com/xxxkjing'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
