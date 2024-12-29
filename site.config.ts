67ad57a85a058fc56d83a554d1232d846d9534f6
MinusTen <xkjing@outlook.com>
2024-12-29 20:07:21 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  //rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'MetaIllusion\'s Blog',
